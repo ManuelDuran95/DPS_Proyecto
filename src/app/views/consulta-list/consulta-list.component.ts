48dcfc8be2fdd5953066b41a43eb51551b07f3a0
Josue2407 <54449197+Josue2407@users.noreply.github.com>
2020-11-24 10:55:12 -0600

    Actualizando Consulta-list

--- src/app/views/consulta-list/consulta-list.component.ts
+++ src/app/views/consulta-list/consulta-list.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import {FormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import Swal from 'sweetalert2';
+import {Consulta} from '../../models/consulta';
+import {ConsultaService} from '../../services/consulta.service';
 
 @Component({
   selector: 'app-consulta-list',
@@ -7,9 +13,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConsultaListComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(public consultaService:ConsultaService) { }
+consultaList:Consulta[];
+  busqueda:string;
+ngOnInit(){
+  return this.consultaService.getdatos()
+  .snapshotChanges().subscribe(item => {
+    this.consultaList = [];
+    item.forEach(element => {
+      let x = element.payload.toJSON();
+      x["$key"] = element.key;
+      this.consultaList.push(x as Consulta);
+    });
+  });
   }
 
+  FiltrarConsultas(){
+    this.consultaList = this.consultaList.filter(data =>{
+    return  data.Propietario.toString().trim()==this.busqueda;
+    })
+
+    if(this.consultaList.length==0){
+      this.consultaService.getdatos()
+    .snapshotChanges().subscribe(item => {
+      this.consultaList = [];
+      item.forEach(element => {
+        let x = element.payload.toJSON();
+        x["$key"] = element.key;
+        this.consultaList.push(x as Consulta);
+      });
+    });
+    Swal.fire({
+      position: 'center',
+      icon: 'info',
+      title: 'Registro No Encontrado',
+      showConfirmButton: false,
+      timer: 1500
+    })
+    }
+    else{
+      this.busqueda ="";
+      Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'Registro Encontrado',
+        showConfirmButton: false,
+        timer: 1500
+      })
+    }
+  }
+ 
 }
